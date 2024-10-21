0b6851367c4c5357419aac68071af2898349176a
autumnnn <autumnnnnn@proton.me>
2024-10-22 00:01:43 +0100

    feat: base main code.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+import config from "./config";
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+client.once(Events.ClientReady, async (c) => {
+  console.info(`Logged in as \`${c.user?.tag}\``);
+});
+
+void client.login(config.token);
