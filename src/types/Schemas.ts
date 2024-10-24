e3811949718f5b804ee182b3b0000f17b7c317b8
autumnnn <autumnnnnn@proton.me>
2024-10-24 18:02:56 +0100

    feat: add types for db schemas.

---
+++ src/types/Schemas.ts
@@ -0,0 +1,31 @@
+export interface GuildSchema {
+  id: number;
+  guild_id: string;
+  created_at: Date;
+}
+
+export interface UserSchema {
+  id: number;
+  user_id: string;
+  guild_id: string;
+  created_at: Date;
+}
+
+export interface BugSchema {
+  id: number;
+  bug_id: number;
+  user_id: number;
+  status: "open" | "closed";
+  title: string;
+  description: string;
+  created_at: Date;
+}
+
+export interface MediaSchema {
+  id: number;
+  media_type: "image" | "video";
+  data: Buffer;
+  user_id: number;
+  bug_id: number;
+  created_at: Date;
+}
